2f35740f8e8ad3116fb729bae11c825c5d52bfd6
laurasofia180 <muozmontoyal@gmail.com>
2017-04-28 15:05:55 -0500

    Añadiedo el wordcount

---
+++ wordcount.py
@@ -0,0 +1,48 @@
+from mongo import Mongo
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+import os
+import collections
+
+WORD_RE = re.compile(r"[\w*]+", re.MULTILINE)
+
+class MRWordFrequencyCount(MRJob):
+
+    def mapper(self, _, doc):
+	name = os.environ["map_input_file"]
+	allnames = name.split('/')
+	name = allnames[len(allnames)-1]
+	H = {}
+	for word in WORD_RE.findall(doc):
+		H[word] = 0
+	for word in WORD_RE.findall(doc):
+		H[word] = H[word]+1
+	for word in H:
+		yield(word,(name,H[word]))
+
+    def reducer(self, word, values):
+	d = collections.defaultdict(int)
+	a = []
+	P = []
+	for n,w in values:
+	    if not n in a: a.append(n)
+    	    d[n]+=w
+	for f in a:
+	    P.append([f,d[f]])
+	P.sort(key=lambda x: x[1],reverse=True)
+
+	top10 = []
+	index = 0
+	for p in P:
+	    if index<10:
+		top10.append(p)
+	    else:
+		break
+	    index+=1;
+
+        yield (word, P)
+
+
+if __name__ == '__main__':
+    MRWordFrequencyCount.run()
