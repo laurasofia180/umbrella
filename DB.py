cd0635d907efbc480b211b584ed78cc453edab11
laurasofia180 <muozmontoyal@gmail.com>
2017-04-28 17:11:00 -0500

    Añadiendo base de datos

--- DB.py
+++ DB.py
@@ -4,3 +4,5 @@ class db ():
   def __init__(self):
         self.client = MongoClient('10.131.137.188')
         self.db = self.client ['Test']
+        self.db = client.test_database
+        self.index = self.db["index"]
