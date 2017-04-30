ad21a19359021abc3bd62d2771247a226c512994
laurasofia180 <muozmontoyal@gmail.com>
2017-04-30 11:46:21 -0500

    Añadiendo a flask la bd

--- DB.py
+++ DB.py
@@ -3,5 +3,10 @@ import MySQLdb
 db = MySQLdb.connect("10.131.137.188") #Abrir la conexion de la base de datos
 cursor = db.cursor()
 cursor.execute("lmunozm")
-cursor.execute ("SELCT * FROM lmunozm") #query para ver la bd 
+cursor.execute ("SELCT * FROM lmunozm") #query para ver la bd
+
+try:
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
 db.close()
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 from wtforms import Form, TextField, SelectField, validators
-#from flask_MySQLdb import MySQLdb
+from flask_mysqldb import MySQL
 
 app = Flask("umbrella") #nuevo objeto
+mysql = MySQL(app)
 
 class SearchForm(Form):
     text= TextField("text",[validators.required()])
@@ -12,7 +13,11 @@ def index ():
     form = SearchForm()
     return render_template("index.html", form=form)
 
- #MySQLdb = None
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT lmunozm, host FROM mysql.lmunozm''')
+    rv = cur.fetchall()
+    return str(rv)
 
 if __name__ == '__main__':
   app.run(debug = True, port = 3000) #Ejecuta el localhost
